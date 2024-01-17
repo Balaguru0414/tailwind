789f80c65ea5370e6bf3d9fb1fd257ab3378f566
L.Balaguru <117264451+Balaguru0414@users.noreply.github.com>
2024-01-17 18:36:20 +0530

    tailwind cdn

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}", "*.html"],
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         "trans-up": {
+//           "0%": { transform: "translateY(-10px)" },
+//         },
+//       },
+//       animation: {
+//         "trans-up": "trans-up 1s ease-in-out infnite alternalte",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
